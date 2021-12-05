fa46aad6d376745ed7dfaa31e5704f1b8f802d6f
Artem Pokamestov <para@fc-zenit.ru>
2021-12-05 12:45:26 +0300

    + Last para 2.12.2021

---
+++ LS-2.0/DicHandling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LS_2._0
+{
+    class DicHandling
+    {
+    }
+}
---
+++ LS-2.0/ModelDB2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LS_2._0
+{
+    public class ModelDB2 : DbContext
+    {
+        // Контекст настроен для использования строки подключения "ModelDB2" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "LS_2._0.ModelDB2" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "ModelDB2" 
+        // в файле конфигурации приложения.
+        public ModelDB2()
+            : base("name=ModelDB2")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
